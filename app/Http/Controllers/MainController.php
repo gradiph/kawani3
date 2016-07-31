8ba0873234663f4ba7449e9c84743ab827b845a7
Gradi <gradiph@gmail.com>
2016-07-31 10:10:10 +0700

    310716 perbaikan fungsi prosesLogin

--- app/Http/Controllers/MainController.php
+++ app/Http/Controllers/MainController.php
@@ -25,29 +25,21 @@ class MainController extends Controller
 	}
 	public function home() {
 		if (Auth::user()) {
-			if (Auth::user()->level=="Admin") {
-				return view('home.admin');
-			}
+//			if (Auth::user()->level=="Admin") {
+//				return view('home.admin');
+//			}
+            return view('home.admin');
 		}
 		else {
 			return Redirect::to('login');
 		}
 	}
 	public function login() {
-		if (Auth::user()) {
-			if (Auth::user()->level=="Admin") {
-				return Redirect::to('home');
-			}
-		}
-		else {
-			return view('home.login');
-		}
+		return view('home.login');
 	}
 	public function prosesLogin(validasilogin $validasi) {
-		if (Auth::attempt(['username' => Input::get('username'), 'password' => Input::get('password')])) {
-			if (Auth::user()->level=="Admin") {
-				return Redirect::to('home');
-			}
+		if (Auth::attempt(['username' => Input::get('username'), 'password' => Input::get('password')],Input::get('remember'))) {
+			return Redirect::to('home');
 		}
 		else {
 			return Redirect::to('login')->with('message','salah username atau salah password');
